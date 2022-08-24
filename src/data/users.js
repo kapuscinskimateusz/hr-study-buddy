6c7374f3a2332af432c95ec7066931922c450eb9
Mateusz Kapuściński <mateusz.kapuscinski@smartbees.co>
2022-08-25 01:19:04 +0200

    add UsersList and UsersListItem components

---
+++ src/data/users.js
@@ -0,0 +1,21 @@
+export const users = [
+  {
+    name: 'Mateusz Kapuściński',
+    attendance: '38%',
+    average: '2.3',
+  },
+  {
+    name: 'Aleksandra Koniczyna',
+    attendance: '23%',
+    average: '3.4',
+  },
+  {
+    name: 'Bartłomiej Jastrząb',
+    attendance: '39%',
+    average: '4.3',
+  },
+  {
+    name: 'Kamil Kostrzewa',
+    average: '3.8',
+  },
+];
